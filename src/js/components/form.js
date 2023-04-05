2e27225d084bb6120c0c852094fe7eddd91ac819
Slava <beslaffko@gmail.com>
2023-04-05 16:19:13 +0500

    Добавил немного валидаций и проверок

--- src/js/components/form.js
+++ src/js/components/form.js
@@ -1,5 +1,4 @@
 const formModule = () => {
-
   const form = document.querySelector('.form')
   const emailField = document.querySelector('.email-field')
   const emailInput = document.querySelector('.email')
@@ -7,16 +6,24 @@ const formModule = () => {
   const nameField = document.querySelector('.name-field')
   const nameInput = document.querySelector('.name')
   const errorName = document.querySelector('.error-name')
+  const success = document.querySelector('.success')
 
   function checkEmail () {
     const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
     if (emailInput?.value?.length < 3) {
-      return errorEmail.innerHTML = `<p>Too shorten email</p>`
+      return(
+        errorEmail.innerHTML = `<p>Too shorten email</p>`,
+        emailInput.style.border = '1px solid red'
+      )
     }
     if (!emailInput.value.match(emailPattern)) {
-      return errorEmail.innerHTML = `<p>Please enter a valid email</p>`
+      return (
+        errorEmail.innerHTML = `<p>Please enter a valid email</p>`,
+        emailInput.style.border = '1px solid red'
+      )
     }
-    emailField.classList.remove('invalid')
+    errorEmail.innerHTML = ''
+    emailInput.style.border = '1px solid'
   }
 
   function checkName (e) {
@@ -34,12 +41,39 @@ const formModule = () => {
         nameInput.style.border = '1px solid red'
       )
     }
+    errorName.innerHTML = ''
+    nameInput.style.border = '1px solid'
   }
+
+  function submitForm () {
+
+    //Формируем данные для отправки на бек
+    const data = {
+      email: emailInput.value,
+      name: nameInput.value,
+    }
+    //Имитируем проверку на отсутствие ошибок
+    if (!errorEmail.hasChildNodes() && !errorName.hasChildNodes()) {
+      success.style.display = 'block'
+      success.innerHTML = 'Successfully sent'
+      form.style.display = 'none'
+    }
+
+  }
+
+  success.addEventListener("click", function() {
+    success.style.display = 'none'
+    form.style.display = 'block'
+    emailInput.value = ''
+    nameInput.value = ''
+  })
+
 
   form.addEventListener('submit', (e) => {
     e.preventDefault()
     checkEmail()
     checkName(e)
+    submitForm()
   })
 
 }
